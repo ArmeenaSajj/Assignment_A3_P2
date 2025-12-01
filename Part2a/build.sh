b8a323c9acdb768e2b0bda93537b29ad9a296004
Armeena <114300299+ArmeenaSajj@users.noreply.github.com>
2025-11-30 22:51:15 -0500

    Update build.sh

--- Part2a/build.sh
+++ Part2a/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-g++ -std=c++17 -Wall -Wextra part2a_101295773_101301357.cpp -o part2a
-echo "Build complete. Run with ./part2a <num_TAs>"
+mkdir -p bin
+
+g++ -std=c++17 -Wall -Wextra part2a_101295773_101301357.cpp -o bin/part2a
+echo "Compiled into bin/part2a"
