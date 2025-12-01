9c4e13d8a2808b6624ca71af83e7b632aafa7d5f
Armeena <114300299+ArmeenaSajj@users.noreply.github.com>
2025-11-30 23:37:25 -0500

    Refactor build script to output executable in current dir

--- Part2a/build.sh
+++ Part2a/build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-mkdir -p bin
-
-g++ -std=c++17 -Wall -Wextra part2a_101295773_101301357.cpp -o bin/part2a
-echo "Compiled into bin/part2a"
+g++ -std=c++17 -Wall -Wextra part2a_101295773_101301357.cpp -o part2a
+echo "Part A built. Run with: ./part2a <num_TAs>"
